8fc5aeb93c82e8f3c1d6824a5b38841525491683
snilth <suchada.nilth@gmail.com>
2024-11-09 16:49:04 +0700

    switch to discord.ext.commands

--- main.py
+++ main.py
@@ -1,28 +1,29 @@
 import discord
+from discord.ext import commands
 
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-# prefix = "-"
+prefix = "-"
 
+# create bot instance
 intents = discord.Intents.default()
 intents.message_content = True
+bot = commands.Bot(command_prefix=prefix,
+                   intents=intents)
 
-client = discord.Client(intents=intents)
-
-@client.event
+@bot.event
 async def on_ready():
-    print(f'{client.user} is now online.')
+    print(f'{bot.user} is now online.')
     print("-----------------------------")
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
-
-client.run(BOT_TOKEN)
+@bot.command()
+async def ping(ctx):
+    user_name = ctx.author.name
+    await ctx.send("pong!")
+    await ctx.send(f"Hello, {user_name}!")
+    
+# run bot
+bot.run(BOT_TOKEN)
