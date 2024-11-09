d537d39138418ea659be57d0890aebd438584470
snilth <suchada.nilth@gmail.com>
2024-11-09 19:42:38 +0700

    add commands: -leave

--- cogs/leave_commands.py
+++ cogs/leave_commands.py
@@ -1,24 +1,25 @@
 from discord.ext import commands
 
-class JoinCommands(commands.Cog):
+class LeaveCommands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         
     @commands.command()
-    async def join(self, ctx):
+    async def leave(self, ctx):
         author = ctx.author
         voice_channel = ctx.author.voice.channel
         
         print(f"user: {author}")
-        print("command: -join")
+        print("command: -leave")
         print(f"voice channel: {voice_channel}")
 
-        await voice_channel.connect()
-        await ctx.send(f"Aya join voice channel: {voice_channel}")
-        print(f"bot join {voice_channel}")
+        await ctx.voice_client.disconnect()
+        await ctx.send(f"Aya leave voice channel: {voice_channel}")
+        print(f"bot leave {voice_channel}")
         
         print("-----------------------------")
-
-async def setup(bot):
-    await bot.add_cog(JoinCommands(bot))
     
+    
+async def setup(bot):
+    await bot.add_cog(LeaveCommands(bot))
+        
